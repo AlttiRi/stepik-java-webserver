1a579db31ab25df03622a2423c647fffb16a6173
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2017-11-29 04:52:39 +0300

    Make the NIO implementation for 7th task

--- L7.0 Homework/src/main/java/main/Main.java
+++ L7.0 Homework/src/main/java/main/Main.java
@@ -14,7 +14,27 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        new Server(new EchoSocketHandlerCreator()).join();
+
+        startThreadBasedEchoServer();
+        //startNIOBasedEchoServer();
+
         System.out.println("Main finished");
+    }
+
+    /**
+     * Вариант через потоки
+     */
+    private static void startThreadBasedEchoServer() throws IOException, InterruptedException {
+        new Server(new EchoSocketHandlerCreator()).join();
+    }
+
+    /**
+     * Вариант с использованием NIO
+     *
+     * Просто эхо-сервер, не парсит входящие сообщения на наличие строки "Bye." todo
+     * В принципе, для задания достаточно автоматического определения, когда клиент сам закрыл сокет.
+     */
+    private static void startNIOBasedEchoServer() {
+        new nio_server.Server(5050).run();
     }
 }
